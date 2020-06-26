ef317ff907daf388eb757385ec3698f334534fa7
yogesharora3 <yogesharora375@gmail.com>
2020-06-26 12:32:40 +0530

    Movie Seat Booking app using js
    in this project i try to use local storage to store the information
    about the number of seats as well  number of seat you selected .

---
+++ README.md
@@ -0,0 +1,14 @@
+# Movie Seat Booking
+
+#### UI displays movie selections and seats in a theater to choose from
+
+### How it works -
+
+- UI dispays the movie list , screen, seats and seat info.
+- User can select a movie and its price
+- User can select and deselect seats
+- User can not select occupied seats
+
+#### Number of seats and price will update as soon as user selects any movie
+
+## Local Storage is being used to store the information about the number of seats and the number of seat you selected previously for particular movie
--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const container = document.querySelector(".container");
 let movie = document.querySelector("#movie");
-const seats = document.querySelectorAll(".seat");
+const seats = document.querySelectorAll(".row .seat");
 const text = document.querySelector(".text");
 const count = document.querySelector("#count");
 const total = document.querySelector("#total");
+const select = document.querySelector("select");
 let ticketPrice = +movie.value;
-function moviePrice() {
-  return parseInt(movie.value);
-}
+
 movie.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
-  updateCount();
+  const seatSelected = document.querySelectorAll(".row .seat.selected");
+  if (seatSelected !== null) {
+    [...seatSelected].forEach((cur, index) => {
+      cur.classList.remove("selected");
+    });
+    count.innerText = 0;
+    total.innerText = 0;
+  }
+  updateUI();
 });
 function updateCount() {
   const seatSelected = document.querySelectorAll(".row .seat.selected");
   const Seatcount = seatSelected.length;
   count.innerText = Seatcount;
   total.innerText = Seatcount * ticketPrice;
+  let seatArray = [...seatSelected].map((arr, index) => {
+    return [...seats].indexOf(arr);
+  });
+  let KeyValue = select.options[select.selectedIndex].innerHTML;
+  localStorage.setItem(KeyValue, JSON.stringify(seatArray));
 }
 container.addEventListener("click", (e) => {
   if (
@@ -27,3 +39,19 @@ container.addEventListener("click", (e) => {
     updateCount();
   }
 });
+
+function updateUI() {
+  let KeyValue = select.options[select.selectedIndex].innerHTML;
+  const seatArray = JSON.parse(localStorage.getItem(KeyValue));
+  if (seatArray !== null && seatArray.length > 0) {
+    seats.forEach((cur, index) => {
+      if (seatArray.indexOf(index) > -1) {
+        cur.classList.add("selected");
+      }
+    });
+    count.innerText = seatArray.length;
+    console.log(ticketPrice);
+    total.innerText = seatArray.length * ticketPrice;
+  }
+}
+window.addEventListener("DOMContentLoaded", updateUI);
